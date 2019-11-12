193b1f34e0228af60c9d178b3b64e9ca8fcaf511
lagrange0125@gmail.com <lagrange0125@gmail.com>
2019-11-12 16:33:37 -0500

    adding lagrange3

---
+++ lagrange3/precache-manifest.18c4d0777d06c779d97cd0117066931c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a58cc0cb4891e47e79e2553acfb87f74",
+    "url": "/lagrange3/index.html"
+  },
+  {
+    "revision": "4b1dffab2aeae4edbf5a",
+    "url": "/lagrange3/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "cee0b4a78d8a10cd3157",
+    "url": "/lagrange3/static/css/main.1e6c2ece.chunk.css"
+  },
+  {
+    "revision": "4b1dffab2aeae4edbf5a",
+    "url": "/lagrange3/static/js/2.56f9049f.chunk.js"
+  },
+  {
+    "revision": "cee0b4a78d8a10cd3157",
+    "url": "/lagrange3/static/js/main.7e4f229c.chunk.js"
+  },
+  {
+    "revision": "6a89f5bc852fe9971f43",
+    "url": "/lagrange3/static/js/runtime-main.397fbc6e.js"
+  }
+]);
